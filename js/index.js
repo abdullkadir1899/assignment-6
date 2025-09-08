efa661cf24277b6887046b39e7182ee0025f600d
abdullkadir1899 <mdabdulkadir1899@gmail.com>
2025-09-08 12:11:19 +0600

    cardContainr modal

--- js/index.js
+++ js/index.js
@@ -1,27 +1,23 @@
-// main card container 
-const cardContainer = document.getElementById('plants-container')
-const modalContainer = document.getElementById('modal-container')
-const categoryContainer = document.getElementById('categoryContainer')
-
-
-
+// main card container
+const cardContainer = document.getElementById("plants-container");
+const modalContainer = document.getElementById("modal-container");
+const categoryContainer = document.getElementById("categoryContainer");
 
 //  all tree & modal
 const loadAllTrees = () => {
-    fetch('https://openapi.programming-hero.com/api/plants')
+  fetch("https://openapi.programming-hero.com/api/plants")
     .then((r) => r.json())
     .then((d) => {
-        const data = d.plants;
-        cardContainer.innerHTML = ''
+      const data = d.plants;
+      cardContainer.innerHTML = "";
 
-        modalContainer.innerHTML= ''
+      modalContainer.innerHTML = "";
 
-        data.forEach((e, index) => {
-            const modalId = `modal_${index}`
-               
-               
-            // cardContainer
-            cardContainer.innerHTML += `
+      data.forEach((e, index) => {
+        const modalId = `modal_${index}`;
+
+        // cardContainer
+        cardContainer.innerHTML += `
             <div class="card bg-base-100 w-96 h-[500px] shadow-sm">
             <figure>
                 <img src="${e.image}">
@@ -41,10 +37,8 @@ const loadAllTrees = () => {
             </div>
             `;
 
-
-
-            // modalContainer
-            modalContainer.innerHTML += `
+        // modalContainer
+        modalContainer.innerHTML += `
             <dialog id="${modalId}" class="modal modal-bottom sm:modal-middle">
             <div class="modal-box">
                 <h3 class="text-lg font-extrabold mb-3">${e.name}</h3>
@@ -60,61 +54,111 @@ const loadAllTrees = () => {
             </div>
             
             </dialog>
-            `
-
-        })
+            `;
+      });
     })
-    .catch((er) => console.log(er))
-
-}
-
-
-
+    .catch((er) => console.log(er));
+};
 
 // category
 const loadCategory = () => {
-    fetch('https://openapi.programming-hero.com/api/categories')
+  fetch("https://openapi.programming-hero.com/api/categories")
     .then((r) => r.json())
     .then((d) => {
-        console.log(d.categories)
-        const categories = d.categories
-        showCategory(categories)
-
+      // console.log(d.categories)
+      const categories = d.categories;
+      showCategory(categories);
     })
-    .catch((er) => console.log(er))
-}
-
-
+    .catch((er) => console.log(er));
+};
 
 const showCategory = (categories) => {
-    cardContainer.innerHTML = ''
-    categories.forEach((e) => {
-            categoryContainer.innerHTML += `
-            <li onclick="loadAllTrees()" id="all-Trees" class="p-2 rounded-lg hover:bg-[#15803d] cursor-pointer hover:text-white font-bold">${e.category_name}</li>
-            `
-        });
+  cardContainer.innerHTML = "";
+  categories.forEach((e) => {
+    categoryContainer.innerHTML += `
+            <li onclick="" id="${e.id}" class="p-2 rounded-lg hover:bg-[#15803d] cursor-pointer hover:text-white font-bold">${e.category_name}</li>
+            `;
+  });
 
-        categoryContainer.addEventListener('click', (e) => {
+  categoryContainer.addEventListener("click", (e) => {
+    const allLI = document.querySelectorAll("li");
 
-            const allLI =document.querySelectorAll('li')
-            
-            allLI.forEach(li => {
-                li.classList.remove('text-white', 'bg-[#15803d]')
-            })
-            
-           if(e.target.localName === 'li'){
-            console.log(e.target)
-            e.target.classList.add('text-white', 'bg-[#15803d]')
-           }
-        })
-}
+    allLI.forEach((li) => {
+      li.classList.remove("text-white", "bg-[#15803d]");
+    });
+
+    if (e.target.localName === "li") {
+      e.target.classList.add("text-white", "bg-[#15803d]");
+      loadCard(e.target.id);
+    }
+  });
+};
+
+const loadCard = (e) => {
+  console.log(e);
+  fetch(`https://openapi.programming-hero.com/api/category/${e}`)
+    .then((r) => r.json())
+    .then((d) => {
+      showCategoryContainer(d.plants);
+    })
+    .catch((err) => console.log(err));
+};
+
+// showCategoryContainer
+const showCategoryContainer = (ar) => {
+  cardContainer.innerHTML = "";
+  modalContainer.innerHTML = "";
+
+  ar.forEach((ele, index) => {
+    const modalId = `modal_${index}`;
+
+    // card
+    cardContainer.innerHTML += `
+        <div class="card bg-base-100 w-96 h-[500px] shadow-sm">
+           <figure>
+                <img src="${ele.image}">
+            </figure>
+         <div class="card-body">
+                    <h2 onclick="document.getElementById('${modalId}').showModal()" class="card-title cursor-pointer">${ele.name}</h2>
+                    <p>${ele.description}</p>
+                    <ul class="flex justify-between">
+                        <li>
+                            <div class="badge badge-outline bg-[#dcfce7] border-none">${ele.category}</div>
+                        </li>
+                        <li>
+                            <div class="text-[#15803d]">৳ ${ele.price}</div>
+                        </li>
+                    </ul>
+                    <div class="card-actions justify-end">
+                        <button class="btn bg-[#15803d] w-full text-white rounded-full">Add to Cart</button>
+                    </div>
+                </div>            
+        </div>
+        `;
 
 
-const loadCard = () => {
-    fetch(`https://openapi.programming-hero.com/api/category/${id}`)
-}
 
 
 
-loadCategory()
-loadAllTrees()
+        
+    // modal
+    modalContainer.innerHTML += `
+            <dialog id="${modalId}" class="modal modal-bottom sm:modal-middle">
+                <div class="modal-box">
+                    <h3 class="text-lg font-extrabold mb-3">${ele.name}</h3>
+                    <img class="h-[300px] w-full rounded-xl" src="${ele.image}"/>
+                    <p class="mt-3">Price: ৳${ele.price}</p>
+                    <p class="py-4 font-bold">${ele.description}</p>
+                    <div class="modal-action">
+                        <form method="dialog">
+                            <button class="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+        `;
+  });
+};
+
+loadCategory();
+loadAllTrees();
